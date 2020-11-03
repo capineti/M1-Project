5bc7509ef141ecd59fb60299fd8d02fbe52b78f0
Sergio <sccapineti@gmail.com>
2020-11-03 15:01:07 +0100

    in the Api file show the files only if there is an image and if this image is a Nike Jordan one

--- scripts/nikeApi.js
+++ scripts/nikeApi.js
@@ -11,10 +11,15 @@ fetch("https://nike-jordan.herokuapp.com/jordans")
   .then((data) => {
     data.forEach((obj) => {
       const newImg = document.createElement("img"); // Creating a tag <img src=""/>
-
-      newImg.setAttribute("src", obj.media.imageUrl);
-      newImg.style.width = "200px";
-      document.body.appendChild(newImg);
+      if (
+        obj.media.imageUrl !== null &&
+        !obj.media.imageUrl.includes("Placeholder") &&
+        obj.shoe.includes("Jordan") //to get the file only is there is an image and if this image is a Nike Jordan one
+      ) {
+        newImg.setAttribute("src", obj.media.imageUrl);
+        newImg.style.width = "200px";
+        document.body.appendChild(newImg);
+      }
     });
   })
   .catch((error) => {
